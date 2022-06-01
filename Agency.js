e7f6608db6619a712ddbb7746d7e3094e40fd3a7
Pauolo <Pauolosavery1@gmail.com>
2022-06-01 20:23:22 +0300

    All done

--- Agency.js
+++ Agency.js
@@ -1,19 +1,28 @@
 class Agency {
   constructor(money, tours = []) {
-
+    this.money = money;
+    this.tours = tours;
   }
 
   availableToursCount() {
-
+    return this.tours.length;
   }
 
   findTour(countryName) {
+    // console.log(this.tours.find((obj) => obj.country === countryName));
+    // console.log(this.tours.indexOf(this.tours.find((obj) => obj.country === countryName)));
 
+    return this.tours.find((obj) => obj.country === countryName);
   }
 
   sell(countryName) {
-
+    return this.findTour(countryName);
   }
 }
 
 module.exports = Agency;
+
+// splice ( indexOf (), 1) == [массив элементов]
+// shift - удаляет 1й элемент массива и возвращает его
+// 1 найти страну-тур = вернуть его индекс
+// 2 индекс существует = вырезать элемент из массива.
--- Tourist.js
+++ Tourist.js
@@ -1,10 +1,19 @@
-class Tourist {
-  constructor() {
+// const Agency = require('../Agency');
 
+class Tourist {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.tours = [];
   }
 
   buy(countryName, agency) {
-
+    const wannaTour = agency.sell(countryName)
+    if (wannaTour) {
+      this.tours.push(wannaTour);
+      return true;
+    }
+    return false;
   }
 }
 
